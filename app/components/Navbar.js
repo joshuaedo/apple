11ff5a51495dd9033d24979b1ee9d4c174bdb59a
Joshua Edo <joshua.edo01@gmail.com>
2023-06-10 16:49:21 +0000

    Update 3 files

--- app/components/Navbar.js
+++ app/components/Navbar.js
@@ -1,22 +1,55 @@
 import Link from "next/link";
+// import Image from "next/image";
+import { appleIcons } from "../image";
 
 export default function Navbar() {
+  const [AppleIcon, SearchIcon, ShoppingBagIcon] = appleIcons;
   return (
-    <div className="bg-[#fbfbfd] opacity-80 flex text-sm text-clip h-11 space-x-5">
-      <Link href="https://apple.com/">Home</Link>
-      <Link href="https://apple.com/">Store</Link>
-      <Link href="https://apple.com/">Mac</Link>
-      <Link href="https://apple.com/">iPad</Link>
-      <Link href="https://apple.com/">iPhone</Link>
-      <Link href="https://apple.com/">Watch</Link>
-      <Link href="https://apple.com/">Vision</Link>
-      <Link href="https://apple.com/">Airpods</Link>
-      <Link href="https://apple.com/">TV & Home</Link>
-      <Link href="https://apple.com/">Entertainment</Link>
-      <Link href="https://apple.com/">Accessories</Link>
-      <Link href="https://apple.com/">Support</Link>
-      <Link href="https://apple.com/">Search</Link>
-      <Link href="https://apple.com/">Bag</Link>
+    <div className="flex">
+      <div className="bg-[#fbfbfd] opacity-80 flex text-sm text-clip h-11 space-x-7 mx-auto whitespace-nowrap px-4 fixed z-10">
+        <Link className="my-auto" href="/">
+          <AppleIcon />
+        </Link>
+        <Link className="my-auto" href="https://apple.com/">
+          Store
+        </Link>
+        <Link className="my-auto" href="https://apple.com/">
+          Mac
+        </Link>
+        <Link className="my-auto" href="https://apple.com/">
+          iPad
+        </Link>
+        <Link className="my-auto" href="https://apple.com/">
+          iPhone
+        </Link>
+        <Link className="my-auto" href="https://apple.com/">
+          Watch
+        </Link>
+        <Link className="my-auto" href="https://apple.com/">
+          Vision
+        </Link>
+        <Link className="my-auto" href="https://apple.com/">
+          Airpods
+        </Link>
+        <Link className="my-auto" href="https://apple.com/">
+          TV & Home
+        </Link>
+        <Link className="my-auto" href="https://apple.com/">
+          Entertainment
+        </Link>
+        <Link className="my-auto" href="https://apple.com/">
+          Accessories
+        </Link>
+        <Link className="my-auto" href="https://apple.com/">
+          Support
+        </Link>
+        <Link className="my-auto" href="https://apple.com/">
+          <SearchIcon />
+        </Link>
+        <Link className="my-auto" href="https://apple.com/">
+          <ShoppingBagIcon />
+        </Link>
+      </div>
     </div>
   );
 }
--- next.config.js
+++ next.config.js
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  experimental: {
+    serverActions: true,
+  },
   images: {
     domains: ["apple.com", "www.apple.com"],
   },
