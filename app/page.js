a7aa92f75b4aae32413df3a29c0200b9010b3759
Joshua Edo <joshua.edo01@gmail.com>
2023-06-16 18:44:40 +0000

    Update page.js

--- app/page.js
+++ app/page.js
@@ -18,7 +18,7 @@ export default function HomePage() {
 
   const backgroundStyles = {
     backgroundPosition: "center",
-    backgroundSize: isMobile ? "140%" : "cover",
+    backgroundSize: "cover",
     backgroundRepeat: "no-repeat",
   };
 
@@ -27,31 +27,29 @@ export default function HomePage() {
       <section
         className="h-[90vh] w-full relative"
         style={{
-          backgroundPosition: "center",
-          backgroundSize: "cover",
-          backgroundRepeat: "no-repeat",
+          ...backgroundStyles,
           backgroundImage: `url(https://www.apple.com/v/home/bb/images/heroes/apple-vision-pro/hero_apple_vision_pro__e8407ehady6i_largetall_2x.jpg)`,
         }}
       >
         <div className="absolute text-center m-auto left-0 right-0 bottom-10 space-y-3 px-3">
           <div className="flex mx-auto items-center justify-center">
             <LargeAppleIcon fill="black" />
-            <h2 className="text-3xl md:text-6xl font-bold opacity-90 inline">
+            <h2 className="text-4xl md:text-6xl font-bold opacity-90 inline">
               Vision Pro
             </h2>
           </div>
-          <p className="text-base md:text-2xl opacity-90">
+          <p className="text-lg md:text-2xl opacity-90">
             Welcome to the era of spatial computing.
           </p>
-          <p className="text-gray-500 text-sm md:text-xl">
+          <p className="text-gray-500 text-base md:text-xl">
             Available early next year in the U.S
           </p>
-          <p className="text-blue-800 text-sm md:text-xl">Learn More &gt;</p>
+          <p className="text-blue-800 text-base md:text-xl">Learn More &gt;</p>
         </div>
       </section>
 
       <section
-        className="h-[83vh] md:h-[90vh] w-full relative bg-cover"
+        className="h-[83vh] md:h-[90vh] w-full relative"
         style={{
           ...backgroundStyles,
           backgroundImage: `url(https://www.apple.com/v/home/bb/images/heroes/iphone-14-pro/hero_iphone14pro_spring__9xo85pm6sbmm${
@@ -61,14 +59,14 @@ export default function HomePage() {
       >
         <div className="absolute text-center m-auto left-0 right-0 top-10 space-y-4 px-3">
           <div className="flex mx-auto items-center justify-center">
-            <h2 className="text-3xl md:text-6xl font-bold opacity-90 inline text-white">
+            <h2 className="text-4xl md:text-6xl font-bold opacity-90 inline text-white">
               iPhone 14 Pro
             </h2>
           </div>
-          <p className="text-base md:text-2xl opacity-90 text-white">
+          <p className="text-lg md:text-2xl opacity-90 text-white">
             Pro. Beyond.
           </p>
-          <p className="text-blue-600 text-sm md:text-xl">
+          <p className="text-blue-600 text-base md:text-xl">
             <span> Learn More &gt; </span>
             <span className="pl-4"> Buy &gt;</span>
           </p>
@@ -86,14 +84,14 @@ export default function HomePage() {
       >
         <div className="absolute text-center m-auto left-0 right-0 top-10 space-y-4 px-3">
           <div className="flex mx-auto items-center justify-center">
-            <h2 className="text-3xl md:text-6xl font-bold opacity-90 inline">
+            <h2 className="text-4xl md:text-6xl font-bold opacity-90 inline">
               MacBook Air 15”
             </h2>
           </div>
-          <p className="text-base md:text-2xl opacity-90">
+          <p className="text-lg md:text-2xl opacity-90">
             Impressively big. Impossibly thin.
           </p>
-          <p className="text-blue-600 text-sm md:text-xl">
+          <p className="text-blue-600 text-base md:text-xl">
             <span> Learn More &gt; </span>
             <span className="pl-4"> Buy &gt;</span>
           </p>
@@ -111,14 +109,14 @@ export default function HomePage() {
         >
           <div className="absolute text-center m-auto left-0 right-0 top-10 space-y-4 px-3">
             <div className="flex mx-auto items-center justify-center">
-              <h2 className="text-3xl md:text-5xl font-bold opacity-90 inline">
+              <h2 className="text-4xl md:text-5xl font-bold opacity-90 inline">
                 MacBook Studio
               </h2>
             </div>
-            <p className="text-base md:text-xl opacity-90">
+            <p className="text-lg md:text-xl opacity-90">
               Supercharged by M2 Max and M2 Ultra.
             </p>
-            <p className="text-blue-600 text-sm md:text-xl">
+            <p className="text-blue-600 text-base md:text-xl">
               <span> Learn More &gt; </span>
               <span className="pl-4"> Buy &gt;</span>
             </p>
@@ -135,14 +133,14 @@ export default function HomePage() {
         >
           <div className="absolute text-center m-auto left-0 right-0 top-10 space-y-4 px-3">
             <div className="flex mx-auto items-center justify-center">
-              <h2 className="text-3xl md:text-5xl font-bold opacity-90 inline">
+              <h2 className="text-4xl md:text-5xl font-bold opacity-90 inline">
                 Mac Pro
               </h2>
             </div>
-            <p className="text-base md:text-xl opacity-90">
+            <p className="text-lg md:text-xl opacity-90">
               Transformed by Apple silicon.
             </p>
-            <p className="text-blue-600 text-sm md:text-xl">
+            <p className="text-blue-600 text-base md:text-xl">
               <span> Learn More &gt; </span>
               <span className="pl-4"> Buy &gt;</span>
             </p>
@@ -160,14 +158,14 @@ export default function HomePage() {
           <div className="absolute text-center m-auto left-0 right-0 top-10 space-y-4 px-3">
             <div className="flex mx-auto items-center justify-center">
               <LargeAppleIcon fill="black" />
-              <h2 className="text-3xl md:text-5xl font-bold opacity-90 inline">
+              <h2 className="text-4xl md:text-5xl font-bold opacity-90 inline">
                 Trade In
               </h2>
             </div>
-            <p className="text-base md:text-xl opacity-90">
+            <p className="text-lg md:text-xl opacity-90">
               Upgrade and save. Its that easy
             </p>
-            <p className="text-blue-600 text-sm md:text-xl">
+            <p className="text-blue-600 text-base md:text-xl">
               <span>See what your device is worth &gt; </span>
             </p>
           </div>
@@ -185,19 +183,19 @@ export default function HomePage() {
             <div className="">
               <div className="flex mx-auto items-center justify-center">
                 <LargeAppleIcon fill="white" />
-                <h2 className="text-3xl md:text-5xl font-bold opacity-90 inline pb-2">
+                <h2 className="text-4xl md:text-5xl font-bold opacity-90 inline pb-2">
                   WATCH
                 </h2>
               </div>
-              <p className="text-sm md:text-base opacity-90 text-red-600">
+              <p className="text-base md:text-base opacity-90 text-red-600">
                 SERIES 8
               </p>
             </div>
 
-            <p className="text-base md:text-xl opacity-90">
+            <p className="text-lg md:text-xl opacity-90">
               A healthy leap ahead.
             </p>
-            <p className="text-blue-600 text-sm md:text-xl">
+            <p className="text-blue-600 text-base md:text-xl">
               <span> Learn More &gt; </span>
               <span className="pl-4"> Buy &gt;</span>
             </p>
@@ -214,15 +212,15 @@ export default function HomePage() {
         >
           <div className="absolute text-center m-auto left-0 right-0 bottom-10 space-y-4 px-3 text-white">
             <div className="flex mx-auto items-center justify-center">
-              <h2 className="text-3xl md:text-4xl font-bold opacity-90 inline px-10 md:px-20">
+              <h2 className="text-4xl md:text-4xl font-bold opacity-90 inline px-10 md:px-20">
                 Racial Equality and Justice Initiative
               </h2>
             </div>
-            <p className="text-base md:text-xl opacity-90 px-10 md:px-20">
+            <p className="text-lg md:text-xl opacity-90 px-10 md:px-20">
               We&apos;re advancing equity by expanding opportunities around the
               world
             </p>
-            <p className="text-blue-600 text-sm md:text-xl">
+            <p className="text-blue-600 text-base md:text-xl">
               <span> Learn More &gt; </span>
             </p>
           </div>
@@ -239,14 +237,14 @@ export default function HomePage() {
           <div className="absolute text-center m-auto left-0 right-0 top-10 space-y-4">
             <div className="flex mx-auto items-center justify-center">
               <LargeAppleIcon fill="black" />
-              <h2 className="text-3xl md:text-5xl font-bold opacity-90 inline">
+              <h2 className="text-4xl md:text-5xl font-bold opacity-90 inline">
                 Card
               </h2>
             </div>
-            <p className="text-base md:text-xl opacity-90 px-20">
+            <p className="text-lg md:text-xl opacity-90 px-20">
               Get up to 3% Daily Cash back with every purchase
             </p>
-            <p className="text-blue-600 text-sm md:text-xl">
+            <p className="text-blue-600 text-base md:text-xl">
               <span className=""> Learn More &gt; </span>
               <span className="pl-4"> Buy &gt;</span>
             </p>
